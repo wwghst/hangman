90a808016965f854ee04921981663b6b44dc2f6a
Arslan <wowghost@bk.ru>
2024-03-15 20:47:03 +0600

    Add rain, sounds, car headlights

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,4 @@
+//rain
 let canvas = document.getElementsByClassName('rain')[0];
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -5,56 +6,87 @@ canvas.height = window.innerHeight;
 let c = canvas.getContext('2d');
 
 function randomNum(max, min) {
-	return Math.floor(Math.random() * max) + min;
+    return Math.floor(Math.random() * max) + min;
 }
 
 function RainDrops(x, y, endy, velocity, opacity) {
 
-	this.x = x;
-	this.y = y;
-	this.endy = endy;
-	this.velocity = velocity;
-	this.opacity = opacity;
+    this.x = x;
+    this.y = y;
+    this.endy = endy;
+    this.velocity = velocity;
+    this.opacity = opacity;
 
-	this.draw = function() {
-		c.beginPath();
-		c.moveTo(this.x, this.y);
-		c.lineTo(this.x, this.y - this.endy);
-		c.lineWidth = 1;
-		c.strokeStyle= "rgba(255, 255, 255, " + this.opacity + ")";
-		c.stroke();
-	}
+    this.draw = function () {
+        c.beginPath();
+        c.moveTo(this.x, this.y);
+        c.lineTo(this.x, this.y - this.endy);
+        c.lineWidth = 1;
+        c.strokeStyle = "rgba(255, 255, 255, " + this.opacity + ")";
+        c.stroke();
+    }
 
-	this.update = function() {
-		let rainEnd = window.innerHeight + 100;
-		if (this.y >= rainEnd) {
-			this.y = this.endy - 100;
-		} else {
-			this.y = this.y + this.velocity;
-		}
-		this.draw();
-	}
+    this.update = function () {
+        let rainEnd = window.innerHeight + 100;
+        if (this.y >= rainEnd) {
+            this.y = this.endy - 100;
+        } else {
+            this.y = this.y + this.velocity;
+        }
+        this.draw();
+    }
 
 }
 
 let rainArray = [];
 
-for (let i = 0; i < 140; i++) {
-	let rainXLocation = Math.floor(Math.random() * window.innerWidth) + 1;
-	let rainYLocation = Math.random() * -500;
-	let randomRainHeight = randomNum(10, 2);
-	let randomSpeed = randomNum(20, .2);
-	let randomOpacity = Math.random() * .55;
-	rainArray.push(new RainDrops(rainXLocation, rainYLocation, randomRainHeight, randomSpeed, randomOpacity));
+for (let i = 0; i < 1540; i++) {
+    let rainXLocation = Math.floor(Math.random() * window.innerWidth) + 1;
+    let rainYLocation = Math.random() * -500;
+    let randomRainHeight = randomNum(30, 5);
+    let randomSpeed = randomNum(20, 5);
+    let randomOpacity = Math.random() * .55;
+    rainArray.push(new RainDrops(rainXLocation, rainYLocation, randomRainHeight, randomSpeed, randomOpacity));
 }
 
 function animateRain() {
 
-	requestAnimationFrame(animateRain);
-	c.clearRect(0,0, window.innerWidth, window.innerHeight);
+    requestAnimationFrame(animateRain);
+    c.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
-	for (let i = 0; i < rainArray.length; i++) {
-		rainArray[i].update();
-	}
+    for (let i = 0; i < rainArray.length; i++) {
+        rainArray[i].update();
+    }
 
 }
+
+animateRain();
+
+
+//audio
+function audioPlay(...audio) {
+    for (let i = 0; i < audio.length; i++) {
+        audio[i].play();
+    }
+}
+
+const rainAudio = new Audio();
+rainAudio.src = '../audio/rain.mp3';
+rainAudio.loop = true;
+rainAudio.volume = 0.6;
+
+
+const carAudio = new Audio();
+carAudio.src = '../audio/car.mp3';
+carAudio.loop = true;
+carAudio.volume = 1.0;
+
+audioPlay(rainAudio, carAudio);
+
+//headlights
+const carHeadlights = document.querySelectorAll('.main__car__headlight');
+const carHeadlightsInterval = setInterval(() => {
+    carHeadlights.forEach(carHeadlight => {
+        carHeadlight.classList.toggle('main__car__headlight--active');
+    })
+}, 3000);
